0012b31c5f85118785cfef7fcf3f97135307b3fe
narumi <narumi@waifu.club>
2021-10-13 03:00:59 +0200

    Binsecure (Partial)

--- src/main/java/uwu/narumi/deobfuscator/transformer/impl/binsecure/latest/BinsecureInvokeDynamicVariableTransformer.java
+++ src/main/java/uwu/narumi/deobfuscator/transformer/impl/binsecure/latest/BinsecureInvokeDynamicVariableTransformer.java
@@ -11,20 +11,10 @@ import java.util.Map;
 
 public class BinsecureInvokeDynamicVariableTransformer extends Transformer {
 
-
-    /*
-        x = index/position
-        y = value
-
-        put(x, y);
-        get(x)
-     */
-
     @Override
     public void transform(Deobfuscator deobfuscator) throws Exception {
         deobfuscator.classes().forEach(classNode -> classNode.methods.forEach(methodNode -> {
             Map<Long, Integer> vars = new HashMap<>();
-            methodNode.maxLocals += 3;
 
             Arrays.stream(methodNode.instructions.toArray())
                     .filter(node -> node instanceof MethodInsnNode)
@@ -32,49 +22,7 @@ public class BinsecureInvokeDynamicVariableTransformer extends Transformer {
                     .filter(node -> node.owner.equals("sun/misc/Unsafe"))
                     .filter(node -> node.name.startsWith("put") || node.name.startsWith("get"))
                     .forEach(node -> {
-                        boolean doubleSize = node.name.endsWith("Long") || node.name.endsWith("Double");
-                        boolean store = node.name.startsWith("put");
-
-                        long key = 0;
-                        if (node.getPrevious() instanceof VarInsnNode && isLong(node.getPrevious().getPrevious().getPrevious())) {
-                            key = getLong(node.getPrevious().getPrevious().getPrevious());
-                            if (!vars.containsKey(key))
-                                vars.put(key, methodNode.maxLocals + (doubleSize ? 2 : 1));
-
-                            methodNode.instructions.remove(node.getPrevious().getPrevious().getPrevious().getPrevious().getPrevious());
-                            methodNode.instructions.remove(node.getPrevious().getPrevious().getPrevious().getPrevious());
-                            methodNode.instructions.remove(node.getPrevious().getPrevious().getPrevious());
-                            methodNode.instructions.remove(node.getPrevious().getPrevious());
-
-                            methodNode.instructions.set(node, new VarInsnNode(getOpcode(node, store), vars.get(key)));
-                        } else if (isLong(node.getPrevious().getPrevious().getPrevious().getPrevious()) || isLong(node.getPrevious().getPrevious())) {
-                            if (node.name.startsWith("put")) {
-                                key += getLong(node.getPrevious().getPrevious().getPrevious().getPrevious());
-                            } else {
-                                key += getLong(node.getPrevious().getPrevious());
-                            }
-
-                            if (!vars.containsKey(key))
-                                vars.put(key, methodNode.maxLocals + (doubleSize ? 2 : 1));
-
-                            if (store) {
-                                if (doubleSize)
-                                    methodNode.instructions.remove(node.getPrevious().getPrevious().getPrevious().getPrevious().getPrevious().getPrevious().getPrevious().getPrevious());
-
-                                methodNode.instructions.remove(node.getPrevious().getPrevious().getPrevious().getPrevious().getPrevious().getPrevious().getPrevious());
-                                methodNode.instructions.remove(node.getPrevious().getPrevious().getPrevious().getPrevious().getPrevious().getPrevious());
-                                methodNode.instructions.remove(node.getPrevious().getPrevious().getPrevious().getPrevious().getPrevious());
-                            }
-
-                            methodNode.instructions.remove(node.getPrevious().getPrevious().getPrevious().getPrevious());
-                            methodNode.instructions.remove(node.getPrevious().getPrevious().getPrevious());
-                            methodNode.instructions.remove(node.getPrevious().getPrevious());
-                            methodNode.instructions.remove(node.getPrevious());
-
-                            methodNode.instructions.set(node, new VarInsnNode(getOpcode(node, store), vars.get(key)));
-                        }
-
-                        methodNode.maxLocals += (doubleSize ? 2 : 1);
+                        //NO
                     });
 
             clean(methodNode);
