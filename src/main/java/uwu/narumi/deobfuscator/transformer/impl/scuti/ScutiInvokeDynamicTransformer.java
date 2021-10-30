f7f5a973abd3c0f8ab09eb48e9485025db58d876
_____Python_____ <73321240+Python-22@users.noreply.github.com>
2021-10-30 10:16:33 -0400

    Added ScutiInvokeDynamic Transformer

---
+++ src/main/java/uwu/narumi/deobfuscator/transformer/impl/scuti/ScutiInvokeDynamicTransformer.java
@@ -0,0 +1,77 @@
+package uwu.narumi.deobfuscator.transformer.impl.scuti;
+
+import org.objectweb.asm.tree.*;
+import uwu.narumi.deobfuscator.Deobfuscator;
+import uwu.narumi.deobfuscator.exception.TransformerException;
+import uwu.narumi.deobfuscator.helper.ASMHelper;
+import uwu.narumi.deobfuscator.transformer.Transformer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author Python 28/oct/2021
+ */
+public class ScutiInvokeDynamicTransformer extends Transformer {
+
+    private int removedMethods = 0;
+
+    @Override
+    public void transform(Deobfuscator deobfuscator) throws Exception {
+        deobfuscator.classes()
+                .forEach(classNode -> classNode.methods.stream()
+                .filter(methodNode -> methodNode.name.length() > 50)
+                .filter(methodNode -> methodNode.desc.equals("(Ljava/lang/String;)Ljava/lang/String;"))
+                .filter(methodNode -> methodNode.access == ACC_PRIVATE + ACC_STATIC + ACC_BRIDGE + ACC_SYNTHETIC)
+                .findFirst()
+                .ifPresent(decrypt -> {
+                    AtomicInteger key = new AtomicInteger(getKey(decrypt));
+
+                    classNode.methods.forEach(methodNode -> Arrays.stream(methodNode.instructions.toArray()).sequential()
+                            .filter(node -> node instanceof InvokeDynamicInsnNode)
+                            .map(InvokeDynamicInsnNode.class::cast)
+                            .filter(node -> node.bsm.getDesc().equals("(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/Integer;)Ljava/lang/invoke/CallSite;"))
+                            .filter(node -> node.bsmArgs.length == 4)
+                            .filter(node -> node.name.length() > 50)
+                            .forEach(node -> {
+                                String owner = decrypt((String) node.bsmArgs[0], key.get()).replace('.', '/');
+                                String name = decrypt((String) node.bsmArgs[1], key.get());
+                                String desc = decrypt((String) node.bsmArgs[2], key.get());
+                                int opcode = (int) node.bsmArgs[3] == 0 ? INVOKESTATIC : INVOKEVIRTUAL;
+
+                                methodNode.instructions.set(node, new MethodInsnNode(opcode, owner, name, desc, false));
+                            }));
+                }));
+
+        deobfuscator.classes().forEach(classNode -> {
+            for (MethodNode methodNode : classNode.methods) {
+                System.out.println(methodNode.desc + " " + methodNode.access);
+                if (methodNode.name.length() > 50 && methodNode.access == 4170 && methodNode.desc.equals("(Ljava/lang/String;)Ljava/lang/String;")) {
+                    classNode.methods.remove(methodNode);
+                    removedMethods++;
+                }
+            }
+        });
+        LOGGER.info("Removed Decryption Functions: " + removedMethods);
+    }
+
+    private int getKey(MethodNode node) {
+        return Arrays.stream(node.instructions.toArray())
+                .filter(ASMHelper::isInteger)
+                .filter(insn -> insn.getNext().getOpcode() == IXOR)
+                .filter(insn -> insn.getNext().getNext().getOpcode() == I2C)
+                .map(ASMHelper::getInteger)
+                .findFirst()
+                .orElseThrow(TransformerException::new);
+    }
+
+    private String decrypt(final String string, final int decryptValue) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < string.length(); i++) {
+            stringBuilder.append((char) (string.charAt(i) ^ decryptValue));
+        }
+        return stringBuilder.toString();
+    }
+}
