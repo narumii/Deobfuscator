38297799e31473d57dc71c98196a33ba8d4f8517
NaulbiMIX <48631163+NaulbiMIX@users.noreply.github.com>
2022-01-05 15:45:25 +0300

    Fix binscure clinit remover

--- src/main/java/uwu/narumi/deobfuscator/transformer/impl/binsecure/old/FuckedClinitRemoveTransformer.java
+++ src/main/java/uwu/narumi/deobfuscator/transformer/impl/binsecure/old/FuckedClinitRemoveTransformer.java
@@ -1,4 +1,4 @@
-package uwu.narumi.deobfuscator.transformer.impl.binsecure.old;
+package uwu.narumi.deobfuscator.transformer.impl.binsecure.latest;
 
 import uwu.narumi.deobfuscator.Deobfuscator;
 import uwu.narumi.deobfuscator.transformer.Transformer;
@@ -7,6 +7,25 @@ public class FuckedClinitRemoveTransformer extends Transformer {
 
     @Override
     public void transform(Deobfuscator deobfuscator) throws Exception {
-        deobfuscator.classes().forEach(classNode -> classNode.methods.removeIf(methodNode -> methodNode.name.equals("<clinit>") && !methodNode.desc.equals("()V")));
+        deobfuscator.classes().forEach(classNode -> classNode.methods.removeIf(methodNode -> {
+            if (methodNode.name.equals("<clinit>")) {
+                if (!methodNode.desc.equals("()V")) {
+             //       System.out.println("C "+ classNode.name +" M "+methodNode.name + " D " + methodNode.desc);
+                    return true;
+                }
+            }
+            return false;
+        }));
+        deobfuscator.getOriginalClasses().forEach((s, classNode) -> classNode.methods.removeIf(methodNode -> {
+            if (methodNode.name.equals("<clinit>")) {
+                if (!methodNode.desc.equals("()V")) {
+                 //   System.out.println("C "+ classNode.name +" M "+methodNode.name + " D " + methodNode.desc);
+                    return true;
+                }
+            }
+            return false;
+        }));
+
+        /*  classNode.methods.removeIf(methodNode -> methodNode.name.equals("<clinit>") && !methodNode.desc.equals("()V"))*/
     }
 }
