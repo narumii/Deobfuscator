d40608e1360839c1e7ec114c33f751b4876b5607
ploot <26153075+uwuplant@users.noreply.github.com>
2022-07-31 01:17:55 -0400

    fix watermark detection

--- src/main/java/uwu/narumi/deobfuscator/transformer/impl/bozoriusz/BozoriuszShitRemoverTransformer.java
+++ src/main/java/uwu/narumi/deobfuscator/transformer/impl/bozoriusz/BozoriuszShitRemoverTransformer.java
@@ -1,6 +1,7 @@
 package uwu.narumi.deobfuscator.transformer.impl.bozoriusz;
 
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
 import uwu.narumi.deobfuscator.Deobfuscator;
 import uwu.narumi.deobfuscator.transformer.Transformer;
 
@@ -9,12 +10,20 @@ import java.util.function.Predicate;
 public class BozoriuszShitRemoverTransformer extends Transformer {
 
     private final Predicate<String> namePredicate = (s -> s.split("\u0001/", 69).length > 3 || s.split("\u0020").length > 3);
-    private final Predicate<ClassNode> contentPredicate = (cn -> cn.fields.isEmpty() && cn.methods.get(0).name.equals("\u0001"));
+    private final Predicate<ClassNode> contentPredicate = (cn -> (cn.methods.size() > 0 && findWatermark(cn)));
+
+    private boolean findWatermark(ClassNode cn) {
+        for (MethodNode method : cn.methods) {
+            if (method.desc.equals("(\u0001/)L\u0001/;"))
+                return true;
+        }
+        return false;
+    }
 
     @Override
     public void transform(Deobfuscator deobfuscator) throws Exception {
-        deobfuscator.classes().removeIf(classNode -> namePredicate.test(classNode.name)
-                || contentPredicate.test(classNode));
+        deobfuscator.classes().removeIf(classNode -> namePredicate.test(classNode.name));
         deobfuscator.getFiles().entrySet().removeIf(entry -> namePredicate.test(entry.getKey()));
+        deobfuscator.classes().removeIf(contentPredicate);
     }
 }
