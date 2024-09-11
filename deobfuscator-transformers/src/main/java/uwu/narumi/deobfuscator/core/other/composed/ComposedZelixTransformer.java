07ed1b788f9d1c624684c193750546a57f661b2f
EpicPlayerA10 <adasko.lesiak@gmail.com>
2024-09-11 20:34:58 +0200

    recover synthetics

--- deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/composed/ComposedZelixTransformer.java
+++ deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/composed/ComposedZelixTransformer.java
@@ -2,7 +2,10 @@ package uwu.narumi.deobfuscator.core.other.composed;
 
 import uwu.narumi.deobfuscator.api.transformer.ComposedTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.clean.peephole.JsrInlinerTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.clean.peephole.PopUnUsedLocalVariablesTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.clean.peephole.UselessPopCleanTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.pool.InlineStaticFieldTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.universal.RecoverSyntheticsTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.universal.UniversalNumberTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.zkm.ZelixLongEncryptionTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.zkm.ZelixUselessTryCatchRemoverTransformer;
@@ -14,13 +17,18 @@ public class ComposedZelixTransformer extends ComposedTransformer {
   public ComposedZelixTransformer() {
     super(
         JsrInlinerTransformer::new,
+        RecoverSyntheticsTransformer::new,
 
         // Fixes flow a bit
         ZelixUselessTryCatchRemoverTransformer::new,
 
         ZelixLongEncryptionTransformer::new,
         InlineStaticFieldTransformer::new,
-        UniversalNumberTransformer::new
+        UniversalNumberTransformer::new,
+
+        // Cleanup
+        PopUnUsedLocalVariablesTransformer::new,
+        UselessPopCleanTransformer::new
     );
   }
 }
---
+++ deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/impl/universal/RecoverSyntheticsTransformer.java
@@ -0,0 +1,20 @@
+package uwu.narumi.deobfuscator.core.other.impl.universal;
+
+import uwu.narumi.deobfuscator.api.asm.ClassWrapper;
+import uwu.narumi.deobfuscator.api.context.Context;
+import uwu.narumi.deobfuscator.api.transformer.Transformer;
+
+public class RecoverSyntheticsTransformer extends Transformer {
+  @Override
+  protected void transform(ClassWrapper scope, Context context) throws Exception {
+    context.classes(scope).forEach(classWrapper -> {
+      classWrapper.methods().forEach(methodNode -> {
+        // Recover by name
+        if (methodNode.name.startsWith("lambda$")) {
+          // Mark as synthetic
+          methodNode.access |= ACC_SYNTHETIC;
+        }
+      });
+    });
+  }
+}
