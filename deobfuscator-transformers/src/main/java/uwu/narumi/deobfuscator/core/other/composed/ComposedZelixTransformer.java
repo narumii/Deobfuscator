e5fe6b9f36839d72b287d6d56fb161a488bb1d10
EpicPlayerA10 <adasko.lesiak@gmail.com>
2024-09-17 18:17:51 +0200

    use PeepholeCleanTransformer in zelix composed transformer

--- deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/composed/ComposedZelixTransformer.java
+++ deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/composed/ComposedZelixTransformer.java
@@ -1,9 +1,8 @@
 package uwu.narumi.deobfuscator.core.other.composed;
 
 import uwu.narumi.deobfuscator.api.transformer.ComposedTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.clean.PeepholeCleanTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.clean.peephole.JsrInlinerTransformer;
-import uwu.narumi.deobfuscator.core.other.impl.clean.peephole.PopUnUsedLocalVariablesTransformer;
-import uwu.narumi.deobfuscator.core.other.impl.clean.peephole.UselessPopCleanTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.pool.InlineStaticFieldTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.universal.RecoverSyntheticsTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.universal.UniversalNumberTransformer;
@@ -34,8 +33,7 @@ public class ComposedZelixTransformer extends ComposedTransformer {
         UniversalNumberTransformer::new,
 
         // Cleanup
-        PopUnUsedLocalVariablesTransformer::new,
-        UselessPopCleanTransformer::new
+        PeepholeCleanTransformer::new
     );
   }
 }
