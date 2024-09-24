cd17888fd4a0b4a2f20e463e344c7fea5e15e473
EpicPlayerA10 <adasko.lesiak@gmail.com>
2024-09-24 19:57:26 +0200

    remove fields with <html>

--- deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/composed/ComposedHP888Transformer.java
+++ deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/composed/ComposedHP888Transformer.java
@@ -4,6 +4,7 @@ import uwu.narumi.deobfuscator.api.transformer.ComposedTransformer;
 import uwu.narumi.deobfuscator.core.other.composed.general.ComposedGeneralFlowTransformer;
 import uwu.narumi.deobfuscator.core.other.composed.general.ComposedGeneralRepairTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.clean.peephole.DeadCodeCleanTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.exploit.WebExploitRemoveTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.hp888.HP888PackerTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.hp888.HP888StringTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.universal.UniversalNumberTransformer;
@@ -18,6 +19,7 @@ public class ComposedHP888Transformer extends ComposedTransformer {
         HP888StringTransformer::new,
         () -> new HP888PackerTransformer(packedEndOfFile),
         HP888StringTransformer::new,
+        WebExploitRemoveTransformer::new,
         ComposedGeneralRepairTransformer::new,
         UniversalNumberTransformer::new,
         ComposedGeneralFlowTransformer::new,
@@ -28,6 +30,7 @@ public class ComposedHP888Transformer extends ComposedTransformer {
   public ComposedHP888Transformer() {
     super(
         HP888StringTransformer::new,
+        WebExploitRemoveTransformer::new,
         ComposedGeneralRepairTransformer::new,
         UniversalNumberTransformer::new,
         ComposedGeneralFlowTransformer::new,
