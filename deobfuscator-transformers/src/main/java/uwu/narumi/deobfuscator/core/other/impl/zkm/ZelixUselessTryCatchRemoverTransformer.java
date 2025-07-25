b16586951b3168f7e7744cf3b053834e4ee1103d
FengeDamc <fengedamc@outlook.com>
2025-07-25 13:42:23 +0800

    ZelixUselessTryCatchRemoverTransformer

--- deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/impl/zkm/ZelixUselessTryCatchRemoverTransformer.java
+++ deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/impl/zkm/ZelixUselessTryCatchRemoverTransformer.java
@@ -1,98 +1,64 @@
 package uwu.narumi.deobfuscator.core.other.impl.zkm;
 
-import org.objectweb.asm.tree.MethodInsnNode;
 import uwu.narumi.deobfuscator.api.asm.InsnContext;
 import uwu.narumi.deobfuscator.api.asm.MethodContext;
-import uwu.narumi.deobfuscator.api.asm.MethodRef;
 import uwu.narumi.deobfuscator.api.asm.matcher.Match;
-import uwu.narumi.deobfuscator.api.asm.matcher.MatchContext;
 import uwu.narumi.deobfuscator.api.asm.matcher.group.SequenceMatch;
-import uwu.narumi.deobfuscator.api.asm.matcher.impl.MethodMatch;
+import uwu.narumi.deobfuscator.api.asm.matcher.impl.InvokeDynamicMatch;
+import uwu.narumi.deobfuscator.api.asm.matcher.impl.NumberMatch;
 import uwu.narumi.deobfuscator.api.asm.matcher.impl.OpcodeMatch;
+import uwu.narumi.deobfuscator.api.asm.matcher.impl.VarLoadMatch;
 import uwu.narumi.deobfuscator.api.transformer.Transformer;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 /**
- * Removes useless try catches. References:
- * <ul>
- * <li>https://www.zelix.com/klassmaster/featuresExceptionObfuscation.html</li>
- * </ul>
- *
  * <pre>
- * {@code
- * try {
- *     ...
- * } catch (PacketEventsLoadFailureException packetEventsLoadFailureException) {
- *     throw PacketEvents.a(packetEventsLoadFailureException);
- * }
- *
- * // Self return
- * private static Exception a(Exception exception) {
- *     return exception;
- * }
- * }
+ *   {@code
+ *   try {
+ *        ...
+ *   }
+ *   catch (RuntimeException runtimeException) {
+ *       throw class_16.method_169("£", (Object)runtimeException, (long)-140537288428847988L, (long)l);
+ *   }
+ *   }
  * </pre>
+ *
  */
-public class ZelixUselessTryCatchRemoverTransformer extends Transformer {
-  private static final Match INSTANT_RETURN_EXCEPTION =
-      SequenceMatch.of(
-          OpcodeMatch.of(ALOAD),
-          OpcodeMatch.of(ARETURN)
-      );
 
-  private static final Match INVOKE_AND_RETURN =
+public class ZelixUselessTryCatchRemoverTransformer extends Transformer {
+
+  private static final SequenceMatch InvokeD_ATHROW =
       SequenceMatch.of(
-          MethodMatch.invokeStatic().capture("invocation"),
+          NumberMatch.of(),
+          VarLoadMatch.of(),
+          InvokeDynamicMatch.create(),
           OpcodeMatch.of(ATHROW)
       );
-
+/*
+LDC -140537288428847988L
+LLOAD a1
+INVOKEDYNAMIC £ (Ljava/lang/Object;JJ)Ljava/lang/RuntimeException; handle[H_INVOKESTATIC cn/cool/cherish/module/impl/combat/友何友树何何树友树何.d(Ljava/lang/invoke/MethodHandles$Lookup;Ljava/lang/String;Ljava/lang/invoke/MethodType;)Ljava/lang/invoke/CallSite;] args[]
+ATHROW
+ */
   @Override
   protected void transform() throws Exception {
     scopedClasses().forEach(classWrapper -> {
-      List<MethodRef> instantReturnExceptionMethods = new ArrayList<>();
-
-      // Find methods that instantly returns an exception
-      classWrapper.methods().forEach(methodNode -> {
-        MethodContext methodContext = MethodContext.of(classWrapper, methodNode);
-
-        // Check instructions
-        if (methodNode.instructions.size() == 2 && INSTANT_RETURN_EXCEPTION.matches(methodContext.at(methodNode.instructions.getFirst()))) {
-          // Add it to list
-          instantReturnExceptionMethods.add(MethodRef.of(classWrapper.classNode(), methodNode));
-        }
-      });
-
-      Set<MethodRef> toRemove = new HashSet<>();
-
-      // Remove try-catches with these instant return exception methods
       classWrapper.methods().forEach(methodNode -> {
         MethodContext methodContext = MethodContext.of(classWrapper, methodNode);
 
         methodNode.tryCatchBlocks.removeIf(tryBlock -> {
           InsnContext start = methodContext.at(tryBlock.handler.getNext());
-          MatchContext result = INVOKE_AND_RETURN.matchResult(start);
-          if (result != null) {
-            MethodRef methodRef = MethodRef.of((MethodInsnNode) result.insn());
-
-            // Check if method is returning an exception instantly
-            if (!instantReturnExceptionMethods.contains(methodRef)) return false;
-
-            toRemove.add(methodRef);
-
+          if(InvokeD_ATHROW.matches(start)){
+            methodNode.instructions.remove(tryBlock.handler.getNext());
+            methodNode.instructions.remove(tryBlock.handler.getNext());
+            methodNode.instructions.remove(tryBlock.handler.getNext());
+            methodNode.instructions.remove(tryBlock.handler.getNext());
             markChange();
+            //System.out.println("TryCatch Block");
             return true;
-          } else {
-            return false;
           }
+          return false;
         });
       });
-
-      // Remove instant return exception methods
-      classWrapper.methods().removeIf(methodNode -> toRemove.contains(MethodRef.of(classWrapper.classNode(), methodNode)));
     });
   }
 }
