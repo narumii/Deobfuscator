6cdf51eb846e0212be4f711fcea803591785b6bb
Dawid <50593784+Animowany@users.noreply.github.com>
2025-08-24 21:14:08 +0200

    composed branchlock

--- deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/composed/ComposedBranchlockTransformer.java
+++ deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/composed/ComposedBranchlockTransformer.java
@@ -1,9 +1,15 @@
 package uwu.narumi.deobfuscator.core.other.composed;
 
 import uwu.narumi.deobfuscator.api.transformer.ComposedTransformer;
+import uwu.narumi.deobfuscator.core.other.composed.general.ComposedGeneralFlowTransformer;
+import uwu.narumi.deobfuscator.core.other.composed.general.ComposedGeneralRepairTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.branchlock.BranchlockCompabilityStringTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.branchlock.BranchlockSaltingTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.clean.peephole.UselessGotosCleanTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.universal.AccessRepairTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.universal.TryCatchRepairTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.universal.UniversalNumberTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.zkm.ZelixUselessTryCatchRemoverTransformer;
 
 public class ComposedBranchlockTransformer extends ComposedTransformer {
   public ComposedBranchlockTransformer() {
@@ -11,7 +17,9 @@ public class ComposedBranchlockTransformer extends ComposedTransformer {
         () -> new ComposedTransformer(true,
             UniversalNumberTransformer::new,
             () -> new BranchlockCompabilityStringTransformer(false),
-            BranchlockSaltingTransformer::new)
+            BranchlockSaltingTransformer::new),
+        ComposedGeneralRepairTransformer::new, // Deletes "Logic Scrambler"
+        ComposedGeneralFlowTransformer::new // Deobfuscates part of flow obfuscation
     );
   }
 }
