14c3cdf212117bc820b8c060bc8b0f391a699bb6
Dawid <50593784+Animowany@users.noreply.github.com>
2025-10-16 21:51:03 +0200

    Added Composed decryption

--- deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/composed/ComposedSuperblaubeereTransformer.java
+++ deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/composed/ComposedSuperblaubeereTransformer.java
@@ -4,11 +4,13 @@ import uwu.narumi.deobfuscator.api.transformer.ComposedTransformer;
 import uwu.narumi.deobfuscator.core.other.composed.general.ComposedPeepholeCleanTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.clean.LocalVariableNamesCleanTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.pool.InlineLocalVariablesTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.sb27.SuperblaubeereInvokeDynamicTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.sb27.SuperblaubeereStringTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.universal.InlinePureFunctionsTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.universal.UniversalFlowTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.universal.UniversalNumberTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.universal.pool.UniversalNumberPoolTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.universal.pool.UniversalStringPoolTransformer;
-import uwu.narumi.deobfuscator.core.other.impl.sb27.SuperblaubeereStringTransformer;
-import uwu.narumi.deobfuscator.core.other.impl.universal.UniversalFlowTransformer;
 
 /**
  * https://github.com/superblaubeere27/obfuscator
@@ -19,14 +21,16 @@ public class ComposedSuperblaubeereTransformer extends ComposedTransformer {
         // Remove var names as they are obfuscated and names are useless
         LocalVariableNamesCleanTransformer::new,
 
+        UniversalNumberTransformer::new,
+        UniversalNumberPoolTransformer::new,
+
         // Fix flow
         UniversalFlowTransformer::new,
 
-        // Inline number pools
-        UniversalNumberPoolTransformer::new,
         // Decrypt strings
         SuperblaubeereStringTransformer::new,
         UniversalStringPoolTransformer::new,
+        SuperblaubeereInvokeDynamicTransformer::new,
 
         InlinePureFunctionsTransformer::new,
         InlineLocalVariablesTransformer::new,
