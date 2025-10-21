7708523a9f045e03070cf36bd3b4ad59589cb555
Dawid <50593784+Animowany@users.noreply.github.com>
2025-10-21 16:30:53 +0200

    ComposedSkidTransformer

--- deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/composed/ComposedSkidTransformer.java
+++ deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/composed/ComposedSkidTransformer.java
@@ -1,11 +1,11 @@
 package uwu.narumi.deobfuscator.core.other.composed;
 
 import uwu.narumi.deobfuscator.api.transformer.ComposedTransformer;
-import uwu.narumi.deobfuscator.core.other.composed.general.ComposedGeneralFlowTransformer;
-import uwu.narumi.deobfuscator.core.other.impl.clean.peephole.NopCleanTransformer;
-import uwu.narumi.deobfuscator.core.other.impl.skidfuscator.SkidFlowTransformer;
-import uwu.narumi.deobfuscator.core.other.impl.skidfuscator.SkidNumberTransformer;
-import uwu.narumi.deobfuscator.core.other.impl.skidfuscator.SkidStringTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.clean.peephole.*;
+import uwu.narumi.deobfuscator.core.other.impl.pool.InlineLocalVariablesTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.pool.InlineStaticFieldTransformer;
+import uwu.narumi.deobfuscator.core.other.impl.skidfuscator.*;
+import uwu.narumi.deobfuscator.core.other.impl.universal.UniversalFlowTransformer;
 import uwu.narumi.deobfuscator.core.other.impl.universal.UniversalNumberTransformer;
 
 public class ComposedSkidTransformer extends ComposedTransformer {
@@ -14,12 +14,25 @@ public class ComposedSkidTransformer extends ComposedTransformer {
       super(
           UniversalNumberTransformer::new,
           SkidNumberTransformer::new,
+          SkidTryCatchRemoveTransformer::new,
+          InlineStaticFieldTransformer::new,
           () -> new ComposedTransformer(true,
+              InlineLocalVariablesTransformer::new, /* Passthrough Hash */
+              SkidFlowTransformer::new, /* Resolve SkidFuscator's Flow */
+
+              UniversalFlowTransformer::new, /* Solve SkidFuscator's Switches/Jumps */
+
               NopCleanTransformer::new,
-              SkidFlowTransformer::new,
-              ComposedGeneralFlowTransformer::new
+              UnUsedLabelCleanTransformer::new,
+              UselessGotosCleanTransformer::new,
+
+              () -> new ComposedTransformer(true,
+                  PopUnUsedLocalVariablesTransformer::new,
+                  UselessPopCleanTransformer::new
+              )
           ),
-          SkidStringTransformer::new
+          SkidStringTransformer::new,
+          SkidCleanTransformer::new
       );
     }
 }
