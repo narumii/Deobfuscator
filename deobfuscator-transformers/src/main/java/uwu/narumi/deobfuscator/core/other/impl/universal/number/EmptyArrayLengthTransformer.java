3f577be68ba156f22b7398b5446e81d48d72e757
SpigotRCE <128710385+SpigotRCE@users.noreply.github.com>
2025-10-25 16:18:07 +0530

    use contexts and frame matches in empty-array-length-transformer and mark changes when made

--- deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/impl/universal/number/EmptyArrayLengthTransformer.java
+++ deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/impl/universal/number/EmptyArrayLengthTransformer.java
@@ -1,49 +1,23 @@
 package uwu.narumi.deobfuscator.core.other.impl.universal.number;
 
-import org.objectweb.asm.tree.*;
-import uwu.narumi.deobfuscator.api.helper.AsmHelper;
+import uwu.narumi.deobfuscator.api.asm.MethodContext;
+import uwu.narumi.deobfuscator.api.asm.matcher.impl.*;
 import uwu.narumi.deobfuscator.api.transformer.Transformer;
 
 public class EmptyArrayLengthTransformer extends Transformer {
-  @Override
-  protected void transform() throws Exception {
-    scopedClasses().parallelStream()
-      .forEach(classWrapper -> classWrapper.methods().parallelStream().forEach(methodNode -> {
-        InsnList list = methodNode.instructions;
-        list.forEach(insn -> {
-          if (insn.getOpcode() == ARRAYLENGTH) {
-            AbstractInsnNode arrayInsn = insn.getPrevious();
-            if (arrayInsn != null && arrayInsn.getOpcode() == NEWARRAY) {
-              AbstractInsnNode sizeInsn = arrayInsn.getPrevious();
-              Integer constValue = getInt(sizeInsn);
-              if (constValue != null) {
-                list.set(insn, AsmHelper.numberInsn(constValue));
-                list.remove(arrayInsn);
-                list.remove(sizeInsn);
-              }
-            }
-          }
+  @Override protected void transform() throws Exception {
+    scopedClasses().forEach(classWrapper -> classWrapper.methods().forEach(methodNode -> {
+      MethodContext methodContext = MethodContext.of(classWrapper, methodNode);
+      OpcodeMatch.of(ARRAYLENGTH)
+        .and(FrameMatch.stack(0,
+          OpcodeMatch.of(NEWARRAY).and(FrameMatch.stack(0, NumberMatch.numInteger().capture("array-length")))))
+        .findAllMatches(methodContext)
+        .forEach(matchContext -> {
+          methodNode.instructions.insert(matchContext.insn(),
+            numberInsn(matchContext.captures().get("array-length").insn().asInteger()));
+          matchContext.removeAll();
+          markChange();
         });
-      }));
-  }
-
-  private Integer getInt(AbstractInsnNode insn) {
-    if (insn instanceof InsnNode) {
-      switch (insn.getOpcode()) {
-        case ICONST_M1: return -1;
-        case ICONST_0: return 0;
-        case ICONST_1: return 1;
-        case ICONST_2: return 2;
-        case ICONST_3: return 3;
-        case ICONST_4: return 4;
-        case ICONST_5: return 5;
-      }
-    } else if (insn instanceof IntInsnNode iinsn) {
-      return iinsn.operand;
-    } else if (insn instanceof LdcInsnNode linsn) {
-      Object cst = linsn.cst;
-      if (cst instanceof Integer) return (Integer) cst;
-    }
-    return null;
+    }));
   }
 }
