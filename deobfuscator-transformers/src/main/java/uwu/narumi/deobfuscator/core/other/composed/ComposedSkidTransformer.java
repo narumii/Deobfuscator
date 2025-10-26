a6132220657205d94bbb348ef8791bc26c352dfe
EpicPlayerA10 <62206933+EpicPlayerA10@users.noreply.github.com>
2025-10-26 20:35:11 +0100

    asd

--- deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/composed/ComposedSkidTransformer.java
+++ deobfuscator-transformers/src/main/java/uwu/narumi/deobfuscator/core/other/composed/ComposedSkidTransformer.java
@@ -13,7 +13,6 @@ import uwu.narumi.deobfuscator.core.other.impl.universal.UniversalNumberTransfor
  * SkidFuscator 2.0.11 Community version
  * https://github.com/skidfuscatordev/skidfuscator-java-obfuscator
  */
-
 public class ComposedSkidTransformer extends ComposedTransformer {
 
     public ComposedSkidTransformer() {
