e09a2780cc90a7fe9a5acdac4230cce52449df36
DataM0del <183248792+DataM0del@users.noreply.github.com>
2025-11-26 07:48:26 -0600

    fix: I commited my bootstrap changes AGAIN, reverted.

--- deobfuscator-impl/src/test/java/Bootstrap.java
+++ deobfuscator-impl/src/test/java/Bootstrap.java
@@ -2,21 +2,22 @@ import java.nio.file.Path;
 import org.objectweb.asm.ClassWriter;
 import uwu.narumi.deobfuscator.Deobfuscator;
 import uwu.narumi.deobfuscator.api.context.DeobfuscatorOptions;
-import uwu.narumi.deobfuscator.core.other.composed.ComposedGruntTransformer;
+import uwu.narumi.deobfuscator.core.other.composed.general.ComposedGeneralFlowTransformer;
 
 public class Bootstrap {
 
   public static void main(String[] args) {
     Deobfuscator.from(
         DeobfuscatorOptions.builder()
-            .inputJar(Path.of("work", "grunt-more.jar")) // Specify your input jar here
+            .inputJar(Path.of("work", "obf-test.jar")) // Specify your input jar here
             //.libraries(Path.of("work", "libs")) // Specify your libraries here if needed
             .transformers(
-                ComposedGruntTransformer::new
+                // Pick your transformers here
+                () -> new ComposedGeneralFlowTransformer()
             )
             .continueOnError()
             .classWriterFlags(ClassWriter.COMPUTE_FRAMES)
             .build()
-    ).start();
+        ).start();
   }
 }
